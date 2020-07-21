2064431ccc920bf819b18aad1c77e76c922d224d
iamhb <its.hari.programmer@gmail.com>
2020-07-21 23:07:34 +0530

    Class 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TamilComponent } from './tamil/tamil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TamilComponent
   ],
   imports: [
     BrowserModule,
